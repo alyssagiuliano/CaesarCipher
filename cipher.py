e2237762d2f1dc4fb1b3db38c071f7f3a36041b7
Alyssa Giuliano <alyssagiuliano@Alyssas-MacBook-Pro.local>
2025-01-23 18:21:52 +1100

    basic caesar cipher functionality

---
+++ cipher.py
@@ -0,0 +1,21 @@
+def caesarCipher(encode_decode, message, shift):
+
+    output = ""
+
+    if encode_decode == "decode":
+        # Makes shift a negative, hence shifts alphabet back and decodes (rather than encodes)
+        shift = shift*-1 
+
+    for char in message:
+        # If character is uppercase, start is 65 (ASCII A), else its 97 (ASCII a)
+        start = 65 if char.isupper() else 97
+
+        # ord(char): converts character into ASCII value
+        # - start: maps value within a range of 0 - 25
+        # + (encode)/ - (decode) shift: shifts the letter in the alphabet
+        # %26: ensure results stay within bounds of alphabet (wraps if past z/Z)
+        # + start: shifts value back to ASCII value
+        # wrapped in chr(): to convert ASCII value back to a character
+        # character then appended to output
+        output += chr((ord(char) - start + shift) % 26 + start)
+    print(output)
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import cipher
+
 def main():
     
-    print("Caesar Cipher Encoder or Decoder!\nEncode or decode a message using Caeser cipher!")
-    encode_decode = input("Encode or decode: ").lower()
-    message = input("Enter your message: ")
-    caeser_shift = int(input("Enter shift (e.g. 3): "))
+    print("Caesar Cipher Encoder or Decoder!\nEncode or decode a message using Caeser cipher")
+
+    encode_decode = input("Encode or decode: ").lower() # User selects encode or decode
+    message = input("Enter message: ") # User enters a message to encode/ decode
+    shift = int(input("Enter shift (between 1 - 25): ")) # User selects a shift
+
+    cipher.caesarCipher(encode_decode, message, shift)
 
 if __name__ == "__main__":
     main()
