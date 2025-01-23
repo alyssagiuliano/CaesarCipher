38b1d014b2765493477dea867ee65cc9fff37e75
Alyssa Giuliano <alyssagiuliano@Alyssas-MacBook-Pro.local>
2025-01-23 18:00:25 +1100

    input set up

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    
+    print("Caesar Cipher Encoder or Decoder!\nEncode or decode a message using Caeser cipher!")
+    encode_decode = input("Encode or decode: ").lower()
+    message = input("Enter your message: ")
+    caeser_shift = int(input("Enter shift (e.g. 3): "))
+
+if __name__ == "__main__":
+    main()
