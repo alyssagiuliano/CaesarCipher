8c55fde7bdc7e7279c97447e4c9d12ada39c443b
Alyssa Giuliano <alyssagiuliano@Alyssas-MacBook-Pro.local>
2025-01-26 08:26:22 +1100

    comments for inputValidation.py

--- caesarCipher.py
+++ caesarCipher.py
@@ -5,15 +5,15 @@ def main():
     
     print("Caesar Cipher Encoder and Decoder!\nEncode or decode a message using Caeser cipher\n")
 
-    # User selects encode or decode (validated to only accept "encode" or "decode")
-    encode_decode = inputValidation.encodeDecodeValidation() 
-    ciphertext_plaintext = "ciphertext" if encode_decode == "decode" else "plaintext"
+    # User selects mode (validated to only accept "encode" or "decode")
+    mode = inputValidation.encodeDecodeValidation() 
+    ciphertext_plaintext = "ciphertext" if mode == "decode" else "plaintext"
 
     message = inputValidation.messageValidation(ciphertext_plaintext) # User enters a message to encode/ decode (validated to ensure a message is entered)
 
     shift = inputValidation.shiftValidation() # User enters a shift (validated to only accept an integer between 1 - 25)
 
-    cipher.caesarCipher(encode_decode, message, shift) # caesarCipher function to encode/ decode message
+    cipher.caesarCipher(mode, message, shift) # caesarCipher function to encode/ decode message
 
 if __name__ == "__main__":
     main()
--- inputValidation.py
+++ inputValidation.py
@@ -1,28 +1,28 @@
 def encodeDecodeValidation():
     while True:
-        encode_decode = input("Encode or decode: ").lower()
-        if encode_decode == "encode" or encode_decode == "decode":
-            return encode_decode
-        else:
-            print("Please enter either encode or decode")
+        mode = input("Encode or decode: ").lower() # Gets user input, asking "Encode or decode:"
+        if mode == "encode" or mode == "decode": # If input is "encode" or "decode"
+            return mode # Return the mode that was input  
+        else: # If input is not "encode" or "decode"
+            print("Please enter either encode or decode") # Ask the user to input either "encode" or "decode" - loops back to input 
 
 def messageValidation(ciphertext_plaintext):
     while True:
-        message = input(f"Enter your {ciphertext_plaintext} message: ")
-        if len(message) != 0:
-            return message
-        else:
-            print(f"Please enter your {ciphertext_plaintext} message")
+        message = input(f"Enter your {ciphertext_plaintext} message: ") # Gets user input, asking "Enter your plaintext/ ciphertext message"
+        if len(message) > 0: # If user input message length is bigger than 0
+            return message # Return the message that was input  
+        else: # If input length is not greater than 0 
+            print(f"Please enter your {ciphertext_plaintext} message") # Ask the user to input a message - loops back to input 
 
 def shiftValidation():
     while True:
         try:
-            shift = int(input("Enter shift (integer between 1 - 25): "))
-            if 1 <= shift <= 25:
-                return shift
-            else:
-                print("Please enter an integer between 1 - 25")
-        except ValueError:
-            print("Please enter an integer between 1 - 25")
+            shift = int(input("Enter shift (integer between 1 - 25): ")) # Gets user input, asking "Enter shift (integer between 1 - 25):"
+            if 1 <= shift <= 25: # If input value is greater than or equal to 1, and less than or rqal to 25
+                return shift # Return the shift value that was input  
+            else: # If input value is less than 1, or greater than 25
+                print("Please enter an integer between 1 - 25") # Ask the user to input an integer between 1 - 25 - loops back to input 
+        except ValueError: # If inout value is not an integer
+            print("Please enter an integer between 1 - 25") # Ask the user to input an integer between 1 - 25 - loops back to input 
 
             
